f66372a46591bb4737aac9726eb8c5833fdcd9b7
Charly Chevalier <charly.chevalier@consensys.net>
2024-02-15 11:32:17 +0100

    feat(ui): forward trackEvent context to confirmation templates

--- ui/pages/confirmations/confirmation/templates/index.js
+++ ui/pages/confirmations/confirmation/templates/index.js
@@ -148,9 +148,17 @@ function getAttenuatedDispatch(dispatch) {
  * @param {Function} t - Translation function.
  * @param {Function} dispatch - Redux dispatch function.
  * @param {object} history - The application's history object.
- * @param {object} data - The data object passed into the template from the confimation page.
+ * @param {object} data - The data object passed into the template from the confirmation page.
+ * @param {object} contexts - Contexts objects passed into the template from the confirmation page.
  */
-export function getTemplateValues(pendingApproval, t, dispatch, history, data) {
+export function getTemplateValues(
+  pendingApproval,
+  t,
+  dispatch,
+  history,
+  data,
+  contexts,
+) {
   const fn = APPROVAL_TEMPLATES[pendingApproval.type]?.getValues;
   if (!fn) {
     throw new Error(
@@ -159,7 +167,7 @@ export function getTemplateValues(pendingApproval, t, dispatch, history, data) {
   }
 
   const safeActions = getAttenuatedDispatch(dispatch);
-  const values = fn(pendingApproval, t, safeActions, history, data);
+  const values = fn(pendingApproval, t, safeActions, history, data, contexts);
   const extraneousKeys = omit(values, ALLOWED_TEMPLATE_KEYS);
   const safeValues = pick(values, ALLOWED_TEMPLATE_KEYS);
   if (extraneousKeys.length > 0) {
