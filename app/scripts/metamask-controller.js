3cdc97cd05087a5d9d61b289fb9038eeb9c88a80
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-02-26 15:36:25 +0000

    Prevent rpc node requests on locked state

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -1345,7 +1345,11 @@ export default class MetamaskController extends EventEmitter {
     this.on('controllerConnectionChanged', (activeControllerConnections) => {
       const { completedOnboarding } =
         this.onboardingController.store.getState();
-      if (activeControllerConnections > 0 && completedOnboarding) {
+      if (
+        activeControllerConnections > 0 &&
+        completedOnboarding &&
+        this.isUnlocked()
+      ) {
         this.triggerNetworkrequests();
       } else {
         this.stopNetworkRequests();
